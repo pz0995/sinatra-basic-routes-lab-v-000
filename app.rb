b3363c8bdaac939272ebba1fa750997c7ab08882
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-10 04:18:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is _____."
+  end
+
+  get '/hometown' do
+    "My hometown is _____."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is _____."
+  end
+
 end
